c470ac71fdd6789f31fb81a8a40b2695f12f59c7
Shu Dong <dongs2011@gmail.com>
2020-05-17 14:30:20 -0700

    better proto and compiled js

--- webapp/src/components/task/TaskList.js
+++ webapp/src/components/task/TaskList.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-import { useWeb3React } from '@web3-react/core'
-import IpfsContext from '../../ipfs/IpfsContext.js'
+import { useWeb3React } from '@web3-react/core';
+import IpfsContext from '../../ipfs/IpfsContext.js';
+
+const model = require('../../proto/model_pb.js');
 
 const TaskList = () => {
   const {ipfs, ipfsId} = React.useContext(IpfsContext);
@@ -10,6 +12,11 @@ const TaskList = () => {
   console.log(ipfs);
   console.log(ipfsId);
   console.log(web3React);
+
+  const task = new model.Task();
+  task.setTitle("Title");
+  console.log(task);
+
   return (
     <div></div>
   );
